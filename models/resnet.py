2f0d8ea5e57f81811eada3cfe84dd4d6fce83b9c
Chirag_Padubidri <Chirag.padubidri@gmail.com>
2022-11-09 16:23:15 +0200

    version 1 - upload

---
+++ models/resnet.py
@@ -0,0 +1,17 @@
+from torchvision.models.resnet import ResNet, BasicBlock
+from torch import device, cuda
+import torch.nn as nn
+import torch.optim as optim
+from torchvision import datasets, transforms
+import torch.nn.functional as F
+import pretrainedmodels
+import torch
+
+device = device('cuda' if cuda.is_available() else 'cpu')
+N_classes = 17036
+class ResNetGeolife(ResNet):
+    def __init__(self):
+        super().__init__(BasicBlock, [3, 4, 6, 3], num_classes=N_classes)
+
+        self.conv1 = nn.Conv2d(20, 64, kernel_size=7, stride=1, padding=3, bias=False)
+
